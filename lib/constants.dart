8cbba70731a6f28df43d97ae74a3ad143acc9937
geekyharsh05 <hp424805@gmail.com>
2023-10-08 00:31:26 +0530

    :sparkles: feat: add test & lib files

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const int appId = 414776962;
+  static const String appSign = "ad57bd26e245d98347f8098984b5fb9122ebc1508337c018d1857a35229ee3e8";
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.brown,
+          title: const Text('Live Streaming app'),
+        ),
+        body: const HomePage(),
+      ),
+    );
+  }
+}
+
+

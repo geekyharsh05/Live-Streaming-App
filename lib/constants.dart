fb83ea7290f4e218dc97501810ca8f2d6275b772
Harsh Vardhan Pandey <hp424805@gmail.com>
2023-10-08 19:32:10 +0530

    :fire: remove: environment variables

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const int appId = 414776962;
-  static const String appSign = "ad57bd26e245d98347f8098984b5fb9122ebc1508337c018d1857a35229ee3e8";
+  static const int appId = Your_AppId;
+  static const String appSign = Your_SignKey;
 }
